5a5a6f88c7f61b3ae990ca75ed1ea02a9d6f7816
junheeLee96 <dlwnsgml203@naver.com>
2025-01-04 22:45:41 +0900

    timePeriods 값 추가

--- app/lib/options.ts
+++ app/lib/options.ts
@@ -13,3 +13,26 @@ export const sortings = [
   { name: "좋아요 순", value: "like" },
   { name: "좋아요 낮은 순", value: "least_likes" },
 ];
+
+export const timePeriods = [
+  {
+    name: "하루",
+    value: "day",
+  },
+  {
+    name: "일주일 전",
+    value: "week",
+  },
+  {
+    name: "한달 전",
+    value: "month",
+  },
+  {
+    name: "일년 전",
+    value: "year",
+  },
+  {
+    name: "전체",
+    value: "whole",
+  },
+];
