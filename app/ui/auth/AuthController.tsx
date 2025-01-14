edad9199fab5d1a850699798d9b498639fd0aa0a
junheeLee96 <dlwnsgml203@naver.com>
2025-01-15 00:16:44 +0900

    login logout 컴포넌트 분리

--- app/ui/auth/AuthController.tsx
+++ app/ui/auth/AuthController.tsx
@@ -7,22 +7,30 @@ export default function AuthController() {
   const { data: session } = useSession();
   return (
     <div className="cursor-pointer">
-      {session?.user.nickname ? (
-        <button onClick={() => signOut({ callbackUrl: "/", redirect: true })}>
-          <span className="text-black dark:text-white hover:text-hover-text dark:hover:text-hover-text">
-            로그아웃
-          </span>
-        </button>
-      ) : (
-        <button>
-          <Link
-            href="/login"
-            className="text-black dark:text-white hover:text-hover-text dark:hover:text-hover-text"
-          >
-            로그인하기
-          </Link>
-        </button>
-      )}
+      {session?.user?.id ? <LogoutButton /> : <LoginButton />}
     </div>
+  );
+}
+
+function LogoutButton() {
+  return (
+    <button onClick={() => signOut({ callbackUrl: "/", redirect: true })}>
+      <span className="text-black dark:text-white hover:text-hover-text dark:hover:text-hover-text">
+        로그아웃
+      </span>
+    </button>
+  );
+}
+
+function LoginButton() {
+  return (
+    <button>
+      <Link
+        href="/login"
+        className="text-black dark:text-white hover:text-hover-text dark:hover:text-hover-text"
+      >
+        로그인하기
+      </Link>
+    </button>
   );
 }
